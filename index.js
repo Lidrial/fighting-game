3e0cf888edf90486732ddaf5eaab10080d0f915f
lidrial <remy-nicolas@live.fr>
2022-12-03 16:43:07 +0100

    player move/jump

---
+++ index.js
@@ -0,0 +1,151 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 1
+
+class Sprite {
+    constructor({ position, velocity }) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 150
+        this.lastKey
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } else {
+            this.velocity.y += gravity
+        }
+        // console.log(this.velocity.y);
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const keys = {
+    q: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    z: {
+        pressed: false
+    }
+}
+
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+
+    player.velocity.x = 0
+
+    if (keys.q.pressed && lastKey === 'q') {
+        player.velocity.x = -1
+    } else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 1
+    }
+}
+
+animate()
+
+window.addEventListener('keydown', (event) => {
+    console.log('--------', event.key, '--------');
+    
+    switch (event.key) {
+
+        // player keys
+        case 'd':
+            keys.d.pressed = true
+            player.lastKey = 'd'
+            break
+        case 'q':
+            keys.q.pressed = true
+            player.lastKey = 'q'
+            break
+        case 'z':
+            player.velocity.y = -10
+            break
+
+        // enemy keys
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            enemy.velocity.y = -10
+            break
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+
+    // player keys
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'q':
+            keys.q.pressed = false
+            break
+        case 'z':
+            keys.z.pressed = false
+            break
+    }
+
+    // enemy keys
+    switch (event.key) {
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            break
+        case 'ArrowUp':
+            keys.ArrowUp.pressed = false
+            break
+    }
+})
