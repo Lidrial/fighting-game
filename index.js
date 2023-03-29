6dd4ad8a8372c4d26f4e68b67e40e4c9327e04db
lidrial <remy-nicolas@live.fr>
2023-03-30 00:05:17 +0200

    finition timer + ajout background

--- index.js
+++ index.js
@@ -8,73 +8,15 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 1
 
-class Sprite {
-    constructor({ position, velocity, color, offset }) {
-        this.position = position
-        this.velocity = velocity
-        this.width = 50
-        this.height = 150
-        this.lastKey
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-            width: 100,
-            height: 50,
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './assets/background.png'
+})
 
-        }
-        this.color = color
-        this.isAttacking,
-        this.health = 100
-    }
-
-    draw() {
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-        // attack box
-        if (this.isAttacking){
-            c.fillStyle = 'green'
-            c.fillRect(
-                this.attackBox.position.x,
-                this.attackBox.position.y,
-                this.attackBox.width,
-                this.attackBox.height
-            )
-
-        }
-    }
-
-    update() {
-        this.draw()
-
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
-
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0
-        } else {
-            this.velocity.y += gravity
-        }
-        // console.log(this.velocity.y);
-    }
-
-    attack() {
-        this.isAttacking = true
-        console.log('attack-start', this.isAttacking)
-        setTimeout(() => {
-            this.isAttacking = false
-            console.log('attack-end', this.isAttacking)
-        }, 100)
-    }
-}
-
-const player = new Sprite({
+const player = new Fighter({
     position: {
         x: 0,
         y: 0
@@ -90,7 +32,7 @@ const player = new Sprite({
     }
 })
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x: 400,
         y: 100
@@ -121,43 +63,7 @@ const keys = {
     }
 }
 
-function rectangularCollision({
-    rectangle1, rectangle2
-}) {
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
-            rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
-            rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
-            rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
 
-    )
-}
-
-function determineWinner({player, enemy}){
-    document.querySelector('#result').style.display = 'flex'
-    if(player.health === enemy.health){
-        document.querySelector('#result').innerHTML = 'Tie'
-    }
-    if(player.health > enemy.health){
-        document.querySelector('#result').innerHTML = 'Red Win'
-    }
-    if(player.health < enemy.health){
-        document.querySelector('#result').innerHTML = 'Blue Win' 
-    }
-}
-
-let timer = 60;
-function decreaseTimer(){
-    if(timer > 0){
-        setTimeout(decreaseTimer, 1000)
-        timer--
-        document.querySelector('#timer').innerHTML = timer
-    }
-
-    if(timer === 0){
-        determineWinner({player, enemy})
-    }
-}
 
 decreaseTimer()
 
@@ -166,6 +72,7 @@ function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    background.update()
     player.update()
     enemy.update()
 
@@ -216,7 +123,7 @@ function animate() {
 
     //end game based on health
     if (enemy.health <= 0 || player.health <= 0){
-        determineWinner({player, enemy})
+        determineWinner({player, enemy, timerId})
     }
 }
 
